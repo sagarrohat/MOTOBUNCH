51bdcb07337bdf41d4e2313a3c056fb62521768a
rohatsagar <rohataliassonusagar@gmail.com>
2021-01-17 18:57:06 +0500

    chapa is good

---
+++ app/src/main/java/com/example/fyp/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.fyp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent mainActivityIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
